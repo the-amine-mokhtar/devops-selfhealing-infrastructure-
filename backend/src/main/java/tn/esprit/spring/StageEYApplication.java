9eed6b2825760e4d2c17ea84cc63a6b38fe4f5c5
amine <amine.mokhtar@esprit.tn>
2026-07-30 09:08:31 +0000

    revert 938cc5ce8bf32f10ea642addcb60249e465851fb
    revert Enabled async support

--- backend/src/main/java/tn/esprit/spring/StageEYApplication.java
+++ backend/src/main/java/tn/esprit/spring/StageEYApplication.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableScheduling
-@EnableAsync
 public class StageEYApplication {
 
 	public static void main(String[] args) {
